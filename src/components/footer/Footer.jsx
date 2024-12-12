4966618d20c75bc26662ad8b560b4ce3adfd478c
rrishika222 <rishikarakta2002@gmail.com>
2024-12-12 23:20:50 +0530

    Removed favicon path from footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Icon from '../logo/favicon.ico'
-
-
 const Footer = () => {
   return (
     <section className="relative overflow-hidden py-10 bg-blue-200 border border-t-2 border-t-black">
